1df03ac244ae06e26a59286554063014d9581894
Houssam LAGHZIL <houssam.laghzil@edu.itescia.fr>
2024-08-26 04:07:32 +0200

    dotenv.config();

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
