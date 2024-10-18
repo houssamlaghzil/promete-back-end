b7f06fa43bf43be3cfeb9ccd9f5fe4d0234800ab
Houssam MAC <hlaghzil@gmail.com>
2024-10-18 09:46:43 +0200

    config

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,8 +1,7 @@
 import admin from "firebase-admin";
+import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' }; // Ajout de l'assertion JSON
 
 // Initialisation de Firebase Admin SDK
-const serviceAccount = JSON.parse(fs.readFileSync('../serviceAccountKey.json', 'utf8'));
-
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
