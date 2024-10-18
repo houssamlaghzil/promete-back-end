645dc9e0bca8f05c159a21612016aef619e9b11f
root <hlaghzil@gmail.com>
2024-10-18 11:02:11 +0200

    enfin

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
-import { getDatabase } from 'firebase/database';
 import admin from 'firebase-admin';
+import path from 'path';  // Importer path
+import { fileURLToPath } from 'url';
+
+// Si vous devez utiliser __dirname dans un module ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Utiliser __dirname pour construire le chemin vers le fichier JSON
 import serviceAccount from '../serviceAccountKey.json' assert { type: "json" };
 
 // Initialisation de Firebase Admin SDK
@@ -9,19 +14,24 @@ if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
         storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-        databaseURL: process.env.FIREBASE_DATABASEURL // Ajoutez la bonne URL ici
+        databaseURL: "https://promete-it-default-rtdb.europe-west1.firebasedatabase.app"
     });
 }
 
 // Initialisation du SDK Firebase Client
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAINE,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-    databaseURL: process.env.REACT_APP_DATABASEURL // Ajoutez cette ligne pour le SDK Client
+    apiKey: "AIzaSyDpLsFlJEk6oA94wrkHlEn8DuOzg67xNxw",
+    authDomain: "promete-it.firebaseapp.com",
+    databaseURL: "https://promete-it-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "promete-it",
+    storageBucket: "promete-it.appspot.com",
+    messagingSenderId: "87937879588",
+    appId: "1:87937879588:web:6cb4fd345332b14e76d2b1",
+    measurementId: "G-2FZ8GSY02W"
 };
 
 const app = initializeApp(firebaseConfig);
